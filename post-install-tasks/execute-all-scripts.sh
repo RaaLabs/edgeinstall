abbf88571b622fc58c1784a862ad868fb9afe378
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-06-30 23:30:48 +0200

    updated paths

--- post-install-tasks/execute-all-scripts.sh
+++ post-install-tasks/execute-all-scripts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
 # list all directories, and sort
 for directory in `ls -d */ | sort -V`;
 do
@@ -7,6 +10,6 @@ do
     for executable in `find ./$directory/*.sh -executable -type f`
     do
         echo "$executable"
-        $executable $PWD/$directory
+        $executable ./$directory
     done;
 done;
