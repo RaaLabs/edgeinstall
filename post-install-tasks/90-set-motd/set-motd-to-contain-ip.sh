b9b8491fa4ce959f68d6642c441c7f5747e980f0
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-07-01 14:02:47 +0200

    updating scripts

--- post-install-tasks/90-set-motd/set-motd-to-contain-ip.sh
+++ post-install-tasks/90-set-motd/set-motd-to-contain-ip.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /etc/NetworkManager/dispatcher.d
 
-cat > /etc/NetworkManager/dispatcher.d/01-motd-ips.sh << 'EOF'
+cat > /etc/NetworkManager/dispatcher.d/01motdips.sh << 'EOF'
 #!/usr/bin/env bash
 
 interface=$1
@@ -15,9 +15,11 @@ fi
 
 echo "The current ip configuration for Wired Connection 1: " $(nmcli -g ip4.address connection show Wired\ connection\ 1) > /etc/motd.d/raalabs.motd
 echo "The current ip configuration for Wired Connection 2: " $(nmcli -g ip4.address connection show Wired\ connection\ 2) >> /etc/motd.d/raalabs.motd
+
+/usr/bin/motd-update.sh
 EOF
 
-chmod +x /etc/NetworkManager/dispatcher.d/01-motd-ips.sh
+chmod +x /etc/NetworkManager/dispatcher.d/01motdips.sh
 
 mkdir -p /etc/motd.d
 
